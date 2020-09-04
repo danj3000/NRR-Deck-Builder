d19ea8c5b4a88a1bae5c0ca261522d810d4db994
Dan Johnson <danj3000@outlook.com>
2020-09-04 15:37:53 +0100

    update color and sort order for first release

--- app/src/main/java/org/anrdigital/rebootdeckbuilder/ViewModel/BrowseCardsViewModel.kt
+++ app/src/main/java/org/anrdigital/rebootdeckbuilder/ViewModel/BrowseCardsViewModel.kt
@@ -6,6 +6,7 @@ import org.anrdigital.rebootdeckbuilder.game.CardList
 import org.anrdigital.rebootdeckbuilder.game.CardPool
 import org.anrdigital.rebootdeckbuilder.game.Format
 import org.anrdigital.rebootdeckbuilder.helper.ISettingsProvider
+import org.anrdigital.rebootdeckbuilder.helper.Sorter
 import java.util.*
 
 class BrowseCardsViewModel(private val cardRepo: CardRepository, settingsProvider: ISettingsProvider) : ViewModel() {
@@ -17,12 +18,14 @@ class BrowseCardsViewModel(private val cardRepo: CardRepository, settingsProvide
     var searchText = ""
     var browseFormat: Format
     fun init() {
-        cardList = mPool.getCards()
+        cardList = mPool.cards
+        Collections.sort(cardList, Sorter.CardSorterByName() )
     }
 
     fun doSearch(searchText: String?) {
         cardList!!.clear()
         cardList!!.addAll(cardRepo.searchCards(searchText, mPool))
+        Collections.sort(cardList, Sorter.CardSorterByName() )
     }
 
     fun updatePackFilter(packFilter: ArrayList<String>, format: Format) {
